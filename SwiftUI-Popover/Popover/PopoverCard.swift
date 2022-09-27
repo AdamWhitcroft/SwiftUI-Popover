0d6d95d1e157dcc2209e3f6cca2f5f3e25432f5a
Adam Whitcroft <adamwhitcroftmini@Adams-Mac-mini.local>
2022-09-27 14:08:13 -0700

    some more notes added

--- SwiftUI-Popover/Popover/PopoverCard.swift
+++ SwiftUI-Popover/Popover/PopoverCard.swift
@@ -23,16 +23,32 @@ struct PopoverCard: View {
             
             /*
              Here's the actual popover card.
+             
+             FAIR WARNING
+             
+             This card will scale to the content
+             inside it (yay), but it has NO concept of adapting
+             if it's taller than your device.
+             
+             I built mine this way, because my popovers would
+             always be pretty small, so I didn't have to worry
+             about it.
+             
+             Your mileage may vary.
             */
             VStack(spacing: 16) {
                 Text("Here's the popover!")
                 
                 Button {
+                    /*
+                     I've slowed the animation down so you can see the effect.
+                     Remove `.speed(0.9)` when you no longer want the slow version.
+                    */
                     withAnimation(.interpolatingSpring(stiffness: 200, damping: 20).speed(0.9)) {
                         isPresented = false
                     }
                 } label: {
-                    Text("Close popover")
+                    Text("Close")
                 }
             }
             .padding()
